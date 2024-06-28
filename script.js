b1c601451ddeaed4d4b983b4fe6f9e52c1c79697
abhiram985 <abhiramiyer22@gmail.com>
2024-06-28 00:12:35 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+//calculator program
+const display = document.getElementById("display");
+
+function appendtodisplay(input){
+    display.value += input;
+}
+function cleardisplay(){
+    display.value = "";
+}
+
+
+function calculate(){
+    display.value = eval(display.value);
+}
